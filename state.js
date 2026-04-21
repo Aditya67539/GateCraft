86c05557d566718e8959cfe5e05190a260d8f2a5
Aditya Chaudhary <aditya67539@gmail.com>
2026-04-21 19:03:38 +0530

    feat: add ghostWire and ghostWireCleanup property that will hold the manual waypoints and waypoints cleanup function

--- state.js
+++ state.js
@@ -2,6 +2,8 @@ export const state = {
   mode: "edit",
   justPlacedFromToolbar: false,
   ghostNode: null,
+  ghostWire: null,
+  ghostWireCleanup: null,
   dragging: null,
   offsetX: 0,
   offsetY: 0,
