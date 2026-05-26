d0f56bfea70b3f84f092e54d8c38d40aed00f01f
Aditya Chaudhary <aditya67539@gmail.com>
2026-05-26 23:04:34 +0530

    fix typo

--- logic/evaluate.js
+++ logic/evaluate.js
@@ -115,9 +115,9 @@ function arraysEqual(a, b) {
  * 
  * @param {CircuitBuilder} circuit - The circuit instance containing the gates, wires, and fanout map 
  */
-export function evaluateOnce(ciruict) {
-  const wires = ciruict.wires;
-  const gates = ciruict.getGates();
+export function evaluateOnce(circuit) {
+  const wires = circuit.wires;
+  const gates = circuit.getGates();
   let changed = false;
   // Update signals from input and clock sources
   for (const wire of wires) {
